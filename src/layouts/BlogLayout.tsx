f01efa5bddb63c4ba087a328b2302750ba5fb997
dongjae <dj8@naver.com>
2025-11-24 18:37:58 +0900

    feat: 레이아웃 설정 변경

--- src/layouts/BlogLayout.tsx
+++ src/layouts/BlogLayout.tsx
@@ -4,29 +4,33 @@ type BlogLayoutProps = {
 
 export default function BlogLayout({ children }: BlogLayoutProps) {
     return (
-        <div className="min-h-screen bg-slate-900 text-slate-50">
-            <header className="border-b border-slate-800">
-                <div className="mx-auto max-w-4xl px-4 py-4 flex items-center justify-between">
-                    <div className="font-semibold text-lg">
+        <div className="min-h-screen min-w-full bg-slate-900 text-slate-50">
+
+            <header
+                className="w-[1200px] h-[300px] mx-auto border-b border-slate-300 bg-[size:100%_100%]"
+                style={{
+                    backgroundImage: "url('/images/main-banner.jpg')",
+                }}
+            >
+                <div className="h-full items-center px-4">
+                    <div className="font-semibold text-lg text-white">
                         myblog<span className="text-sky-400">.</span>
                     </div>
-                    <nav className="flex gap-4 text-sm text-slate-300">
-                        <button className="hover:text-sky-400">블로그</button>
-                        <button className="hover:text-sky-400">사진</button>
-                        <button className="hover:text-sky-400">소개</button>
-                    </nav>
                 </div>
             </header>
 
-            <main className="mx-auto max-w-4xl px-4 py-8">
-                {children}
+            <main className="w-[1200px] px-4 py-8">
+                {/*{children}*/}
+                <div className="h-[1500px] bg-white text-gray-800 border border-dashed border-gray-400 p-4 mt-8">
+                    테스트 콘텐츠 (스크롤 확인용)
+                </div>
             </main>
 
-            <footer className="border-t border-slate-800">
-                <div className="mx-auto max-w-4xl px-4 py-4 text-xs text-slate-500">
+            <footer className="mx-auto w-[1200px] border-t border-slate-300 bg-slate-900">
+                <div className="px-4 py-4 text-xs text-slate-500">
                     {new Date().getFullYear()} myblog. All rights reserved
                 </div>
-                </footer>
+            </footer>
         </div>
     );
 }
